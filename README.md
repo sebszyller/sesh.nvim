ba3306a4318330c360176869ed5fe4b6b7348130
Sebastian Szyller <github@sebszyller.com>
2024-11-22 14:52:55 +0200

    feat: skip useless buffers

--- README.md
+++ README.md
@@ -26,6 +26,7 @@ Customise the default `opts`:
 opts = {
     index_dir = vim.fn.stdpath("data") .. "/sesh_index", -- where to save the session files
     max_files = 1000, -- show warning when there are more than max_files session files
+    useless = { "gitcommit", "gitrebase" }, -- do not save these buffers
     verbose = false, -- print INFO messages
 }
 ```
@@ -41,8 +42,12 @@ require("sesh").file_name(path) -- if you want to build around sesh
 ```
 
 *I* like to trigger `save_sesh` on `VimLeavePre`, and have a map for `load_sesh`.
+The recommended way of using `sesh.nvim` is by passing `path = vim.fn.getcwd()`.
+
+### Under the hood
 
 `sesh.nvim` uses the [djb2](https://theartincode.stanis.me/008-djb2/) hash function to name the saved session files.
-The recommended way of using `sesh.nvim` is by passing `path = vim.fn.getcwd()`.
 Given long paths, and varied dir names, you *should never* run into hash collisions but they *might* happen.
 
+Under the hood, `sesh.nvim` uses regular vim sessions.
+It saves only listed buffers with defined buffer/file types.
--- lua/sesh/init.lua
+++ lua/sesh/init.lua
@@ -7,12 +7,14 @@ M.opts = {}
 local defaults = {
 	index_dir = vim.fn.stdpath("data") .. "/sesh_index", -- where to save the session files
 	max_files = 1000, -- show warning when there are more than max_files session files
+	useless = { "gitcommit", "gitrebase" }, -- do not save these buffers
 	verbose = false, -- print INFO messages
 }
 
 function M.setup(opts)
 	M.opts.index_dir = opts.index_dir or defaults.index_dir
 	M.opts.max_files = opts.max_files or defaults.max_files
+	M.opts.useless = opts.useless or defaults.useless
 	M.opts.verbose = opts.verbose or defaults.verbose
 	vim.fn.mkdir(M.opts.index_dir, "p")
 end
@@ -48,21 +50,64 @@ local function maybe_sesh_file(path)
 	end
 end
 
+local function is_useless(buf)
+	return vim.tbl_contains(M.opts.useless, vim.bo[buf].filetype)
+		or vim.bo[buf].buftype ~= ""
+		or vim.api.nvim_buf_get_name(buf) == ""
+end
+
+local function close_useless_bufs()
+	local buffers = vim.api.nvim_list_bufs()
+	local useful = 0
+	for _, buf in ipairs(buffers) do
+		if
+			not vim.api.nvim_buf_is_valid(buf)
+			or not vim.api.nvim_buf_get_option(buf, "buflisted")
+			or is_useless(buf)
+		then
+			local name = vim.api.nvim_buf_get_name(buf)
+			if M.opts.verbose then
+				print(
+					string.format(
+						"Closing buffer %d: name <%s> buftype<%s> filetype<%s>",
+						buf,
+						name,
+						vim.bo[buf].buftype,
+						vim.bo[buf].filetype
+					)
+				)
+			end
+			vim.api.nvim_buf_delete(buf, { force = true })
+		else
+			useful = useful + 1
+		end
+	end
+	return useful
+end
+
 function M.save_sesh(path)
 	-- TODO: do more than notify?
 	local files_num = vim.fn.len(vim.fn.globpath(M.opts.index_dir, "*", 0, 1))
 	if files_num > M.opts.max_files then
 		vim.notify(
-			"There are " .. files_num .. " (>" .. M.opts.max_files .. ") session files already.",
+			string.format("There are %d (>%d) session files already.", files_num, M.opts.max_files),
 			vim.log.levels.WARN
 		)
 	end
 
-	local sesh_file = M.file_name(path)
-	if M.opts.verbose then
-		vim.notify("Saving session file to " .. sesh_file, vim.log.levels.INFO)
+	local useful = close_useless_bufs()
+
+	if useful > 0 then
+		local sesh_file = M.file_name(path)
+		if M.opts.verbose then
+			vim.notify("Saving session file to " .. sesh_file, vim.log.levels.INFO)
+		end
+		vim.cmd("silent! mksession! " .. sesh_file)
+	else
+		if M.opts.verbose then
+			vim.notify("Not enough buffers; skipping saving. ", vim.log.levels.INFO)
+		end
 	end
-	vim.cmd("mksession! " .. sesh_file)
 end
 
 function M.load_sesh(path)
@@ -70,7 +115,7 @@ function M.load_sesh(path)
 	if sesh_file == nil then
 		vim.notify("No session file found for " .. path, vim.log.levels.WARN)
 	else
-		vim.cmd("source " .. sesh_file)
+		vim.cmd("silent! source " .. sesh_file)
 		if M.opts.verbose then
 			vim.notify("Loaded session file from " .. sesh_file, vim.log.levels.INFO)
 		end
